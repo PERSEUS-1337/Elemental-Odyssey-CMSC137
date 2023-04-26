319af706bd7ea1e281d06836c08ab943894265ae
gtopinio <mctopinio@up.edu.ph>
2023-04-26 21:13:51 +0800

    feature: add initial template design for main gui, which includes a title, play button, join button, about button, settings button, and exit button

---
+++ 137-Project-AB4L/src/application/MainGUIController.java
@@ -0,0 +1,49 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainGUIController {
+
+    @FXML
+    private Button btnAbout;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    private Button btnJoin;
+
+    @FXML
+    private Button btnPlay;
+
+    @FXML
+    private Button btnSettings;
+
+    @FXML
+    void onAboutButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onExitButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onJoinButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPlayButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSettingsButtonClicked(ActionEvent event) {
+
+    }
+
+}
--- 137-Project-AB4L/src/user/Main.java
+++ 137-Project-AB4L/src/user/Main.java
@@ -1,5 +1,33 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+// This is a Java platformer game where characters can only interact with specific level elements. 
+// Players are forced to work together in order to accomplish the puzzles. The concept and levels will be heavily 
+// influenced by Fireboy and Watergirl.
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public void start(Stage mainStage){
+        // Get the FXML file for the main menu from the 'views' folder
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/views/MainGUI.fxml"));
+            Scene scene = new Scene(root);
+
+            mainStage.setTitle("Harmony of the Elements");
+            mainStage.setScene(scene);
+            mainStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+	}
+
 }
