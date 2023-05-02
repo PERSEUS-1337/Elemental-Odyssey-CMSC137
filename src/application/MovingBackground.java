eb1ecd7ea7cfd06a383a2f0307e46e4eef05ef8a
gtopinio <mctopinio@up.edu.ph>
2023-05-02 16:41:37 +0800

    update: create background folder in assets, add new colored background images, import new colors to moving background class, add color pick feature for moving background

--- src/application/MovingBackground.java
+++ src/application/MovingBackground.java
@@ -7,8 +7,54 @@ import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
 public class MovingBackground extends Pane {
-    public MovingBackground() {
-        Image bgImg = new Image(getClass().getResource("/assets/bg.png").toExternalForm());
+
+    // Colored images for the background
+    public final Image blueBg = new Image(getClass().getResource("/assets/background/blue.png").toExternalForm());
+    public final Image brownBg = new Image(getClass().getResource("/assets/background/brown.png").toExternalForm());
+    public final Image grayBg = new Image(getClass().getResource("/assets/background/gray.png").toExternalForm());
+    public final Image greenBg = new Image(getClass().getResource("/assets/background/green.png").toExternalForm());
+    public final Image pinkBg = new Image(getClass().getResource("/assets/background/pink.png").toExternalForm());
+    public final Image purpleBg = new Image(getClass().getResource("/assets/background/purple.png").toExternalForm());
+    public final Image yellowBg = new Image(getClass().getResource("/assets/background/yellow.png").toExternalForm());
+
+    public static final String blueColor = "blue";
+    public static final String brownColor = "brown";
+    public static final String grayColor = "gray";
+    public static final String greenColor = "green";
+    public static final String pinkColor = "pink";
+    public static final String purpleColor = "purple";
+    public static final String yellowColor = "yellow";
+
+
+    public MovingBackground(String color) {
+
+        Image bgImg = null;
+        switch (color) {
+            case MovingBackground.blueColor:
+                bgImg = blueBg;
+                break;
+            case MovingBackground.brownColor:
+                bgImg = brownBg;
+                break;
+            case MovingBackground.grayColor:
+                bgImg = grayBg;
+                break;
+            case MovingBackground.greenColor:
+                bgImg = greenBg;
+                break;
+            case MovingBackground.pinkColor:
+                bgImg = pinkBg;
+                break;
+            case MovingBackground.purpleColor:
+                bgImg = purpleBg;
+                break;
+            case MovingBackground.yellowColor:
+                bgImg = yellowBg;
+                break;
+            default:
+                break;
+        }
+
         ImageView background1 = new ImageView(bgImg);
         ImageView background2 = new ImageView(bgImg);
 
