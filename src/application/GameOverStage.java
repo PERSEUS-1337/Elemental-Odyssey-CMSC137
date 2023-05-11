7abb336c40302064f1ea615b3e35fc8480b18b49
gtopinio <mctopinio@up.edu.ph>
2023-05-11 18:40:47 +0800

    fix: add functionality to the close button instead of imageview in gameover stage

--- src/application/GameOverStage.java
+++ src/application/GameOverStage.java
@@ -1,19 +1,19 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
 
 public class GameOverStage {
     public static final Integer WINDOW_WIDTH = 600;
     public static final Integer WINDOW_HEIGHT = 410;
 
     @FXML
-    private ImageView btnClose;
+    private Button btnClose;
 
     @FXML
-    void onCloseButtonClicked(MouseEvent event) {
-
+    void onCloseButtonClicked(ActionEvent event) {
+        System.out.println("Close Button Clicked");
     }
 
 }
